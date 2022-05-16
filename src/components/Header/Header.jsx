8ce6178823f61d2486523bf8ebea963d83c18ace
RockStar666by <savchuk799@gmail.com>
2022-05-16 13:15:27 +0300

    fix: header styles refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Navigation } from '../Navigation/Navigation';
-import LogoImage from '../../assets/a-logo.svg';
 import { CurrencySwitcher } from '../../feature/Switcher/CurrencySwitcher/CurrencySwitcher';
 import { CartButton } from '../Cart/CartButton';
-
-const HeaderWrapper = styled.header`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 80px;
-  max-width: 1440px;
-  box-sizing: border-box;
-  padding: 0 100px;
-  background: white;
-`;
-
-const Logo = styled.img`
-  position: absolute;
-  width: 41px;
-  height: 41px;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-`;
-
-const Actions = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  right: 0;
-`;
-
-Logo.defaultProps = { src: LogoImage };
+import * as Styles from './styles';
 
 export class Header extends React.Component {
   render() {
     return (
-      <HeaderWrapper className="header-wrapper">
+      <Styles.HeaderWrapper className="header-wrapper">
         <Navigation />
-        <Logo />
-        <Actions>
+        <Styles.Logo />
+        <Styles.Actions>
           <CurrencySwitcher />
           <CartButton />
-        </Actions>
-      </HeaderWrapper>
+        </Styles.Actions>
+      </Styles.HeaderWrapper>
     );
   }
 }
