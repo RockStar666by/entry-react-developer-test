2a5aa69dec75561463d460ce0e6cbf80fad8f256
RockStar666by <savchuk799@gmail.com>
2022-05-16 12:38:47 +0300

    fix: image-slider styles refactor

--- src/feature/ImageSlider/Slider.jsx
+++ src/feature/ImageSlider/Slider.jsx
@@ -1,66 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import sliderArrow from '../../assets/slider-arrow.svg';
-
-const BigContainer = css`
-  margin: 24px;
-  height: 100%;
-  width: 200px;
-`;
-
-const SmallContainer = css`
-  margin: 0 0 0 4px;
-  height: 100%;
-  width: 120px;
-`;
-
-const SliderContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  ${(props) => (props.mini ? `${SmallContainer}` : `${BigContainer}`)};
-  overflow: hidden;
-`;
-
-const SliderImage = styled.img`
-  position: absolute;
-  height: 100%;
-  width: auto;
-  opacity: 0;
-  transition: opacity ease-in-out 0.4s;
-  ${(props) => props.isActive && 'opacity: 1;'}
-`;
-
-const SliderButtons = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  height: 24px;
-  width: 56px;
-  bottom: 16px;
-  right: 16px;
-  ${(props) => props.imageCount < 2 && 'display: none;'}
-`;
-
-const SliderButton = styled.button`
-  position: relative;
-  padding: 0;
-  width: 24px;
-  height: 24px;
-  border: none;
-  cursor: pointer;
-  background: rgba(0, 0, 0, 0.73);
-  ${(props) => props.disabled && 'background: rgba(0, 0, 0, 0.3);'}
-  &::before {
-    display: flex;
-    content: '';
-    width: 24px;
-    height: 24px;
-    background: url(${sliderArrow}) center no-repeat;
-    ${(props) => props.right && 'transform: scaleX(-1);'}
-  }
-`;
+import * as Styles from './styles';
 
 export class Slider extends React.PureComponent {
   constructor(props) {
@@ -97,18 +37,18 @@ export class Slider extends React.PureComponent {
     const { currentImageIndex } = this.state;
     console.log(currentImageIndex);
     return (
-      <SliderContainer mini={mini}>
+      <Styles.SliderContainer mini={mini}>
         {/* eslint-disable-next-line */}
         {gallery.map((elem, index) => {
           console.log(index, currentImageIndex);
-          return <SliderImage key={elem} src={elem} isActive={index === currentImageIndex} />;
+          return <Styles.SliderImage key={elem} src={elem} isActive={index === currentImageIndex} />;
         })}
 
-        <SliderButtons imageCount={gallery.length}>
-          <SliderButton disabled={this.handleDisabledLeftButton()} onClick={this.handleClickLeftButton} />
-          <SliderButton disabled={this.handleDisabledRightButton()} right onClick={this.handleClickRightButton} />
-        </SliderButtons>
-      </SliderContainer>
+        <Styles.SliderButtons imageCount={gallery.length}>
+          <Styles.SliderButton disabled={this.handleDisabledLeftButton()} onClick={this.handleClickLeftButton} />
+          <Styles.SliderButton disabled={this.handleDisabledRightButton()} right onClick={this.handleClickRightButton} />
+        </Styles.SliderButtons>
+      </Styles.SliderContainer>
     );
   }
 }
