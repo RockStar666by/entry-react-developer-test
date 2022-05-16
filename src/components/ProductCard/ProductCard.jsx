f7d78aa073b297d3b7b8a9de18d82116a1381421
RockStar666by <savchuk799@gmail.com>
2022-05-16 13:02:56 +0300

    fix: product-card styles refactor

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,133 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled, { ThemeProvider, keyframes } from 'styled-components';
-import { Link } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import { addToCart } from '../../redux/actions';
-import CartIcon from '../../assets/cart-icon-white.svg';
 import { Counter } from '../../feature/Counter/ProductCardCounter/Counter';
 import { CustomButton } from '../../feature/CustomButton/CustomButton';
 import { ParamSwitcher } from '../../feature/Switcher/ParamSwitcher/ParamSwitcher';
+import * as Styles from './styles';
 
 const theme = { primary: '#5ece7b' };
-
-const ProductCardContainer = styled.div`
-  position: relative;
-  width: 386px;
-  height: 444px;
-  display: flex;
-  flex-direction: column;
-  background: white;
-  font-size: 18px;
-  line-height: 30px;
-  overflow: hidden;
-  ${(props) => props.isCartClicked && 'filter: drop-shadow(0px 4px 35px rgba(168, 172, 176, 0.19))'};
-  &:hover {
-    filter: drop-shadow(0px 4px 35px rgba(168, 172, 176, 0.19));
-    .cart-button {
-      opacity: 1;
-    }
-  }
-`;
-
-const ProductCardBox = styled(Link)`
-  text-decoration: none;
-  color: black;
-`;
-
-const ProductImage = styled.div`
-  margin: 16px;
-  height: 330px;
-  width: 354px;
-  background: url(${(props) => props.bgImage}) center no-repeat;
-  background-size: auto 100%;
-  border: none;
-`;
-
-const ProductDescription = styled.div`
-  margin: 8px 16px;
-  height: 58px;
-  width: 354px;
-`;
-
-const ProductHeader = styled.h4`
-  font-weight: 300;
-  margin: 0;
-`;
-
-const ProductPrice = styled.p`
-  font-weight: 500;
-  margin: 0;
-`;
-
-const ProductBadge = styled.div`
-  position: relative;
-  top: 8px;
-  width: 61px;
-  height: 35px;
-  color: white;
-  font-size: 12px;
-  font-weight: 600;
-  line-height: 35px;
-  text-align: center;
-  background: ${(props) => props.theme.primary};
-  opacity: 0;
-`;
-
-const CartButton = styled.button`
-  cursor: pointer;
-  position: absolute;
-  width: 52px;
-  height: 52px;
-  right: 30px;
-  top: 320px;
-  border: none;
-  border-radius: 50px;
-  background: url(${CartIcon}) center no-repeat;
-  background-size: 24px;
-  background-color: ${(props) => props.theme.primary};
-  filter: drop-shadow(0px 4px 11px rgba(29, 31, 34, 0.1));
-  opacity: 0;
-  transition: opacity 0.1s ease-in-out;
-}`;
-
-const slideInFromBottom = keyframes` 
-  0% {
-    transform: translateY(100%);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
-
-const CartOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  z-index: 10;
-  width: 386px;
-  height: 444px;
-`;
-
-const CartInfoContainer = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: calc(100%);
-  min-height: 100px;
-  background: white;
-  box-sizing: border-box;
-  padding: 16px;
-  filter: drop-shadow(0px 4px 11px rgba(29, 31, 34, 0.1));
-  animation: 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s 1 ${slideInFromBottom};
-`;
-
-const CartHeader = styled.h2`
-  margin: 0;
-  height: 46px;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 46px;
-`;
-
 export class ProductCardTemplate extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -177,11 +58,11 @@ export class ProductCardTemplate extends React.PureComponent {
     console.log(id, addedProducts, this.state, currency);
     return (
       <ThemeProvider theme={theme}>
-        <ProductCardContainer isCartClicked={isCartClicked} ref={this.cartRef}>
+        <Styles.ProductCardContainer isCartClicked={isCartClicked} ref={this.cartRef}>
           {isCartClicked && (
-            <CartOverlay>
+            <Styles.CartOverlay>
               {inStock ? (
-                <CartInfoContainer>
+                <Styles.CartInfoContainer>
                   {[...attributes]
                     .sort((b, a) => a.type.localeCompare(b.type))
                     .map((attribute) => (
@@ -198,26 +79,26 @@ export class ProductCardTemplate extends React.PureComponent {
                   <CustomButton wide actionOnClick={() => this.props.addToCart({ id, name, brand, options, quantity, prices })}>
                     ADD TO CART
                   </CustomButton>
-                </CartInfoContainer>
+                </Styles.CartInfoContainer>
               ) : (
-                <CartInfoContainer>
-                  <CartHeader>OUT OF STOCK</CartHeader>
+                <Styles.CartInfoContainer>
+                  <Styles.CartHeader>OUT OF STOCK</Styles.CartHeader>
                   <CustomButton wide>ADD TO WISHLIST</CustomButton>
-                </CartInfoContainer>
+                </Styles.CartInfoContainer>
               )}
-            </CartOverlay>
+            </Styles.CartOverlay>
           )}
-          <ProductCardBox to={`/product/${id}`}>
-            <ProductImage bgImage={gallery[0]}>
-              <ProductBadge>-50%</ProductBadge>
-            </ProductImage>
-            <ProductDescription>
-              <ProductHeader>{`${brand} ${name}`}</ProductHeader>
-              <ProductPrice>{`${prices[currency.index].currency.symbol} ${prices[currency.index].amount}`}</ProductPrice>
-            </ProductDescription>
-          </ProductCardBox>
-          <CartButton className="cart-button" onClick={this.handleCartClicked} />
-        </ProductCardContainer>
+          <Styles.ProductCardBox to={`/product/${id}`}>
+            <Styles.ProductImage bgImage={gallery[0]}>
+              <Styles.ProductBadge>-50%</Styles.ProductBadge>
+            </Styles.ProductImage>
+            <Styles.ProductDescription>
+              <Styles.ProductHeader>{`${brand} ${name}`}</Styles.ProductHeader>
+              <Styles.ProductPrice>{`${prices[currency.index].currency.symbol} ${prices[currency.index].amount}`}</Styles.ProductPrice>
+            </Styles.ProductDescription>
+          </Styles.ProductCardBox>
+          <Styles.CartButton className="cart-button" onClick={this.handleCartClicked} />
+        </Styles.ProductCardContainer>
       </ThemeProvider>
     );
   }
