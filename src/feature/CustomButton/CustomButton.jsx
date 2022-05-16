71cceb579e385761fdb3253017b939cf44c4a125
RockStar666by <savchuk799@gmail.com>
2022-05-16 12:27:24 +0300

    fix: custom-button styles refactor

--- src/feature/CustomButton/CustomButton.jsx
+++ src/feature/CustomButton/CustomButton.jsx
@@ -1,49 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import * as Styles from './styles';
 
 const theme = { primary: '#5ece7b' };
-
-const BigButton = css`
-  width: 292px;
-  height: 52px;
-  line-height: 45px;
-  font-size: 18px;
-  padding: 0 22px;
-`;
-
-const SmallButton = css`
-  width: 140px;
-  height: 45px;
-  line-height: 45px;
-  font-size: 18px;
-  padding: 0 22px;
-`;
-
-const ButtonContainer = styled.button`
-  cursor: pointer;
-  margin: 0 auto;
-  font-weight: 400;
-  font-family: inherit;
-  text-align: center;
-  border: none;
-  color: white;
-  ${(props) => (props.small ? `${SmallButton}` : `${BigButton}`)};
-  width: ${(props) => props.wide && '100%'};
-  background: ${(props) => props.theme.primary};
-  &:hover {
-    filter: drop-shadow(0px 4px 11px rgba(29, 31, 34, 0.1));
-  }
-`;
-
 export class CustomButton extends React.Component {
   render() {
     const { children, small, filled, actionOnClick, wide } = this.props;
     return (
       <ThemeProvider theme={theme}>
-        <ButtonContainer small={small} filled={filled} wide={wide} onClick={actionOnClick}>
+        <Styles.ButtonContainer small={small} filled={filled} wide={wide} onClick={actionOnClick}>
           {children}
-        </ButtonContainer>
+        </Styles.ButtonContainer>
       </ThemeProvider>
     );
   }
